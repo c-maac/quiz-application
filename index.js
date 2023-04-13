cd72242fc50d44cf07ba0dcb1cd0b776b79d6c68
ceren <ceren.evcioglu@outlook.com>
2023-04-12 23:34:36 -0400

    implement route to fetch questions

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fetch = require('node-fetch')
 
 app.use(express.static('public'))
 
 app.listen(port, () => {
   console.log(`Quiz application listening on port ${port}`)
+})
+
+app.get('/questions', async (request,response) => {
+
+  const apiResponse = await fetch('https://opentdb.com/api.php?amount=1')
+  const jsonResponse = await apiResponse.json()
+  response.json(jsonResponse);
+
+  //TODO Error handling - other than 0 response means error -
 })
 
 
--- public/script.js
+++ public/script.js
@@ -1,1 +1,9 @@
 console.log("script.js loaded")
+
+getQuestions()
+
+async function getQuestions() {
+    const response = await fetch('/questions')
+    const data = await response.json(response);
+    console.log(data)
+}
