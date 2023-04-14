7a9ce8d819b02d3194c279419f8f1171f785e306
ceren <ceren.evcioglu@outlook.com>
2023-04-14 00:34:56 -0400

    fix eslint errors

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const fetch = require('node-fetch')
+const express = require('express');
 
-app.use(express.static('public'))
+const app = express();
+const port = 3000;
+const fetch = require('node-fetch');
+
+app.use(express.static('public'));
 
 app.listen(port, () => {
-  console.log(`Quiz application listening on port ${port}`)
-})
+  console.log(`Quiz application listening on port ${port}`);
+});
 
-app.get('/questions', async (request,response) => {
-
-  const apiResponse = await fetch('https://opentdb.com/api.php?amount=1')
-  const jsonResponse = await apiResponse.json()
+app.get('/questions', async (request, response) => {
+  const apiResponse = await fetch('https://opentdb.com/api.php?amount=1');
+  const jsonResponse = await apiResponse.json();
   response.json(jsonResponse);
 
-  //TODO Error handling - other than 0 response means error -
-})
-
-
+  // TODO Error handling - other than 0 response means error -
+});
