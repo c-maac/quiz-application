a623597d3af47a055da58699e6aa5d6c15e0ec9b
ceren <ceren.evcioglu@outlook.com>
2023-05-23 19:19:48 -0400

    handle api error

--- index.js
+++ index.js
@@ -14,6 +14,4 @@ app.get('/questions', async (request, response) => {
   const apiResponse = await fetch('https://opentdb.com/api.php?amount=1');
   const jsonResponse = await apiResponse.json();
   response.json(jsonResponse);
-
-  // TODO Error handling - other than 0 response means error -
 });
--- public/scripts/questionApi.js
+++ public/scripts/questionApi.js
@@ -2,7 +2,10 @@
 const API = {
   getQuestion: () => fetch('/questions')
     .then((res) => res.json())
-    .then((data) => data.results[0]) // TODO: Promise<SpeechRecognitionResult> ?
+    .then((data) => {
+      if (data.response_code !== 0) console.error('Unable to fetch question');
+      return data.results[0];
+    })
     .then(({ question, correct_answer: answer, incorrect_answers: incorrectAnswers }) => ({
       choiceCount: incorrectAnswers.length + 1,
       question,
